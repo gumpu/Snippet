96de391da394d71e9695abe0c1dccafee654e66b
Frans Slothouber <rfsber@xs4all.nl>
2013-05-02 21:28:57 +0200

    Javascript Arrays

---
+++ Javascript/Arrays/array_use.js
@@ -0,0 +1,12 @@
+// vi: spell spl=en
+
+
+Planet = function( x, y ) {
+    this.x = x;
+    this.y = y;
+}
+
+planets = [ new Planet(10,20), new Planet(88,3), new Planet(5,11), new Planet(1,3) ];
+
+console.log( planets );
+
