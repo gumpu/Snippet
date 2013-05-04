26a5b452386d4b19b275f1847037bce5bd0c4a29
Frans Slothouber <rfsber@xs4all.nl>
2013-05-04 21:43:18 +0200

    Javascript Variable use

---
+++ Javascript/Variables/variable_use.js
@@ -0,0 +1,42 @@
+
+
+var foo = { x:1, y:3 };
+
+var bar = foo; 
+// bar and foo now point (name?) to the same object.
+
+console.log( foo );
+console.log( bar );
+
+// When we change bar we also change foo.
+bar.x = 1000;
+bar.z = 10;
+console.log( foo );
+console.log( bar );
+
+FF = function ( par ) {
+    return {
+        fun1 : function() { par.x = 33; },
+        fun2 : function() { par.y = 99; }
+    };
+}( bar );
+
+FF.fun1();
+console.log( bar );
+
+FF.fun2();
+console.log( bar );
+
+
+FF2 = function ( par ) {
+    return {
+        fun1 : function() { par.x = 33; },
+        fun2 : function() { par.y = 99; },
+        fun3 : function() { console.log( par ) }
+    };
+};
+
+ff2 = FF2( {x:1,y:1} );
+ff2.fun1();
+ff2.fun3();
+
